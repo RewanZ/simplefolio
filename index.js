6480ba86291e86bef02bf48af2b3b91a962d7ceb
RewanZ <103199952+RewanZ@users.noreply.github.com>
2024-03-06 13:16:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import initScrollReveal from "./src/scripts/scrollReveal.js";
-import initTiltEffect from "./src/scripts/tiltAnimation.js";
+import initTiltEffect from "./src/scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./src/data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
